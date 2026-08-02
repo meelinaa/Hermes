b715a6942bb664e6cb7e1e9f5152c3bcce0b4db6
Your Name <you@example.com>
2026-08-02 12:57:03 +0200

    refactor: remove redundant blank lines in worker program.cs

--- Hermes.Worker/Program.cs
+++ Hermes.Worker/Program.cs
@@ -1,103 +1,51 @@
 using Hangfire;
-
 using Hermes.Application.Options;
-
 using Hermes.Application.Scheduling;
-
 using Hermes.Worker.Hosting;
-
 using Hermes.Worker.Scheduling;
-
 using Microsoft.Extensions.DependencyInjection;
-
 using Serilog;
-
-
 
 WorkerSerilogBootstrapService.InitializeBootstrapLogger();
 
-
-
 try
-
 {
-
     HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
 
-
-
     builder.UseHermesWorkerSerilog();
-
     builder.AddHermesWorkerOpenTelemetry();
-
-
 
     builder.AddHermesWorker();
 
-
-
     var newsletterOpts = new NewsletterOptions();
-
     builder.Configuration.GetSection(NewsletterOptions.SECTION_NAME).Bind(newsletterOpts);
-
     TimeZoneInfo hangfireNewsletterTz = NewsletterSchedulingProvider.ResolveTimeZone(newsletterOpts.TimeZoneId);
-
-
 
     IHost host = builder.Build();
 
-
-
     using (IServiceScope scope = host.Services.CreateScope())
-
     {
-
         JobStorage? storage = scope.ServiceProvider.GetService<JobStorage>();
-
         if (storage is not null)
-
             JobStorage.Current = storage;
-
     }
-
-
 
     host.LogMailHogDevHints();
 
-
-
     RecurringJob.AddOrUpdate<NewsletterSchedulerWorkerService>(
-
         RecurringJobConstants.ID,
-
         scheduler => scheduler.RunAsync(CancellationToken.None),
-
         Cron.Minutely(),
-
         new RecurringJobOptions { TimeZone = hangfireNewsletterTz });
 
-
-
     host.Run();
-
 }
-
 catch (Exception ex)
-
 {
-
     Log.Fatal(ex, "Hermes.Worker terminated unexpectedly");
-
     throw;
-
 }
-
 finally
-
 {
-
     Log.CloseAndFlush();
-
 }
-
-
